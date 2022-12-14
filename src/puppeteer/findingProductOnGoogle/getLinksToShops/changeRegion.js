e1738829a9e1d3985efe13d779fddc85ed0aa8f8
Szymon Zieba <ssajzmonq@gmail.com>
2022-12-15 00:08:32 +0100

    dsadas

--- src/puppeteer/findingProductOnGoogle/getLinksToShops/changeRegion.js
+++ src/puppeteer/findingProductOnGoogle/getLinksToShops/changeRegion.js
@@ -1,8 +1,13 @@
 export const changeRegion = async(page) => {
+    await page.on('dialog', async dialog => {
+        await dialog.accept()
+    })
     await page.click("#regionanchormore")
-    await page.waitForSelector("#regionoPL")
+    await page.waitForSelector("#regionoRU")
     await page.setExtraHTTPHeaders({
         'Accept-Language': 'pl'
     });
-    await page.click('#regionoPL > div')
+    await page.click('#regionoRU > div')
+    await page.waitForSelector("#form-buttons > div.goog-inline-block.jfk-button.jfk-button-action")
+    await page.click("#form-buttons > div.goog-inline-block.jfk-button.jfk-button-action")
 }
