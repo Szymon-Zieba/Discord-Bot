931413cc23d737a79cc944a576f07e547a9dc6a4
Szymon-Zieba <ssajzmonq@gmail.com>
2023-02-12 17:57:07 +0100

    last changes

--- src/config.js
+++ src/config.js
@@ -1,9 +1,5 @@
 export const browserQuantity = 2
-export const token = "OTU2MzI1NzUxNzE0NjE5NDQy.GuJp5o.VOitO0zsql8avqoq_wa8_teExRhPTIaYj36z0k"
-export const userID = "479057655835656202"
 export const serverID = "956328544861036604"
 export const isBrowserClose = true
-export const googleURL = "https://google.com"
-export const prefGoogleURL = "https://www.google.com/preferences?hl&prev"
 export const domainStartURL = "https://www.google.com/search?q="
 export const domainEndURL = "&tbm=shop"
